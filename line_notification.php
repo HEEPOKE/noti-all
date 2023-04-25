756ed3bf49172b0fee4d77b258d2d8418ac112eb
HEEPOKE <Damon1fx@gmail.com>
2023-04-25 15:57:13 +0700

    :money_with_wings: line notify message

---
+++ line_notification.php
@@ -0,0 +1,25 @@
+<?php
+
+$token = "uwosgjtnlhgOvqEQglZULGj9BhA09nVru8UR5HgD4vK";
+$message = "ติ๊กคอนจ๊อบดิ๊";
+
+$curl = curl_init();
+
+$response = curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://notify-api.line.me/api/notify',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => 'message=' . $message,
+    CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer ' . $token,
+        'Content-Type: application/x-www-form-urlencoded'
+    ),
+));
+
+$response = curl_exec($curl);
+curl_close($curl);
